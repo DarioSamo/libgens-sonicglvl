cabb5cd80c02aec30a6d1acc6381783d03ef2732
dariosamo <dariosamo@gmail.com>
2015-06-22 17:47:40 -0300

    Base for Archive Tree class

---
+++ src/LibGens/ArchiveTree.cpp
@@ -0,0 +1,52 @@
+#include "ArchiveTree.h"
+
+namespace LibGens {
+	ArchiveFile::ArchiveFile() {
+	}
+
+	ArchiveFile::~ArchiveFile() {
+	}
+
+	ArchiveTreeNode::ArchiveTreeNode() {
+		name = "Unnamed";
+		archive = "UnnamedArchive";
+		def_append = "Unnamed";
+	}
+
+	ArchiveTreeNode::~ArchiveTreeNode() {
+	}
+
+	ArchiveTree::ArchiveTree() {
+
+	}
+
+	ArchiveTree::~ArchiveTree() {
+
+	}
+
+	void ArchiveTree::load(string filename) {
+		TiXmlDocument doc(filename);
+		doc.LoadFile();
+
+		TiXmlHandle hDoc(&doc);
+		TiXmlHandle hRoot(0);
+		TiXmlElement *pElem=hDoc.FirstChildElement().Element();
+		for(pElem; pElem; pElem=pElem->NextSiblingElement()) {
+			string entry_name = pElem->ValueStr();
+			printf("%s\n", entry_name.c_str());
+		}
+	}
+
+
+	ArchiveSystem::ArchiveSystem() {
+	}
+
+	ArchiveSystem::~ArchiveSystem() {
+	}
+
+	void ArchiveSystem::buildFromTreeNode(ArchiveTree *tree, string node_name) {
+	}
+
+	ArchiveFile *ArchiveSystem::findFile(string filename) {
+	}
+}
---
+++ src/LibGens/ArchiveTree.h
@@ -0,0 +1,43 @@
+#pragma once
+
+namespace LibGens {
+	class ArchiveFile {
+	protected:
+		string file_path;
+	public:
+		ArchiveFile();
+		~ArchiveFile();
+	};
+
+	class ArchiveTreeNode {
+	protected:
+		string name;
+		string archive;
+		string def_append;
+		list<ArchiveTreeNode *> append_nodes;
+	public:
+		ArchiveTreeNode();
+		~ArchiveTreeNode();
+	};
+
+	class ArchiveTree {
+	protected:
+		list<ArchiveTreeNode *> nodes;
+	public:
+		ArchiveTree();
+		~ArchiveTree();
+
+		void load(string filename);
+	};
+
+	class ArchiveSystem {
+	protected:
+		list<ArchiveFile *> files;
+	public:
+		ArchiveSystem();
+		~ArchiveSystem();
+
+		void buildFromTreeNode(ArchiveTree *tree, string node_name);
+		ArchiveFile *findFile(string filename);
+	};
+};
